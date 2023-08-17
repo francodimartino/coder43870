5fbfaa08c22c927c4afea2964be3fe9c51b29bb3
FrancoDiMartino <francogdimartino@gmail.com>
2023-08-17 20:33:17 -0300

    fin clase 22

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Curso, Profesor
+from .models import Curso, Profesor, Estudiante
 from django.http import HttpResponse
 from .forms import CursoForm, ProfesorForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def crear_curso(request):
@@ -37,15 +39,75 @@ def profesores(request):
             profesion=info["profesion"]
             profesor=Profesor(nombre=nombre,apellido=apellido,email=email,profesion=profesion)
             profesor.save()
-            formulario_profesor=ProfesorForm()
-            return render(request,"AppCoder/profesores.html", {"mensaje":"Profesor creado", "formulario":formulario_profesor})
+            mensaje="Profesor creado"
+            
+            
         else:
-            return render(request,"AppCoder/profesores.html", {"mensaje":"Datos invalidos"})
+            mensaje="Datos invalidos"
+        profesores=Profesor.objects.all()
+        formulario_profesor=ProfesorForm()
+        return render(request,"AppCoder/profesores.html", {"mensaje":mensaje, "formulario":formulario_profesor, "profesores":profesores})
     else:
 
         formulario_profesor=ProfesorForm()
+        profesores=Profesor.objects.all()
 
-    return render(request,"AppCoder/profesores.html", {"formulario":formulario_profesor})
+    return render(request,"AppCoder/profesores.html", {"formulario": formulario_profesor, "profesores":profesores})
+
+def eliminarProfesor(request, id):
+    profesor=Profesor.objects.get(id=id)
+    profesor.delete()
+    profesores=Profesor.objects.all()
+    formulario_profesor=ProfesorForm()
+    mensaje="Profesor eliminado"
+    return render(request,"AppCoder/profesores.html", {"mensaje":mensaje, "formulario":formulario_profesor, "profesores":profesores})
+
+def editarProfesor(request, id):
+
+    profesor=Profesor.objects.get(id=id)
+    if request.method=="POST":
+        form=ProfesorForm(request.POST)
+        if form.is_valid():
+            info=form.cleaned_data
+            profesor.nombre=info["nombre"]
+            profesor.apellido=info["apellido"]
+            profesor.email=info["email"]
+            profesor.profesion=info["profesion"]
+           
+            profesor.save()
+            mensaje="Profesor editado!"
+            profesores=Profesor.objects.all()
+            formulario_profesor=ProfesorForm()
+            return render(request,"AppCoder/profesores.html", {"mensaje":mensaje, "formulario":formulario_profesor, "profesores":profesores})
+
+    else:
+        
+        formulario_profesor=ProfesorForm(initial={"nombre":profesor.nombre, "apellido":profesor.apellido, "email":profesor.email, "profesion":profesor.profesion})
+        return render(request,"AppCoder/editarProfesor.html", {"formulario":formulario_profesor, "profesor":profesor})
+
+
+class EstudianteList(ListView):#vista usada para LISTAR
+    model= Estudiante
+    template_name= "AppCoder/estudiantes.html"
+
+class EstudianteCreacion(CreateView):#vista usada para CREAR
+    model= Estudiante
+    success_url= reverse_lazy("estudiante_list")
+    fields=['nombre', 'apellido', 'email']
+
+class EstudianteDetalle(DetailView): #vista usada para MOSTRAR DATOS
+    model=Estudiante
+    template_name="Appcoder/estudiante_detalle.html"
+
+class EstudianteDelete(DeleteView):#vista usada para ELIMINAR
+    model=Estudiante
+    success_url= reverse_lazy("estudiante_list")
+
+class EstudianteUpdate(UpdateView):#vista usada para EDITAR
+    model = Estudiante
+    success_url = reverse_lazy('estudiante_list')
+    fields=['nombre', 'apellido', 'email']
+
 
 def cursos(request):
     if request.method=="POST":
